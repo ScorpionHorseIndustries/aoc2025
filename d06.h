453d81cd7a2212f7edb96c0b8b4d76f315704a35
ScorpionHorseIndustries <sam.rae0@gmail.com>
2025-12-16 14:18:12 +0000

    d06

--- d06.h
+++ d06.h
@@ -1,11 +1,38 @@
 #pragma once
 
 #include "common.h"
+class D06b {
+    public:
+    char* data;
+
+    void run() {
+        loadData();
+    }
+    void loadData() {
+        std::ifstream file;
+        file.open("./input/d06.txt", std::ios::in | std::ios::binary);
+
+        file.seekg(0, std::ios::end);
+
+        size_t file_size_bytes = file.tellg();
+
+        data = (char*) malloc(sizeof(char) * file_size_bytes);
+        file.seekg(0,std::ios::beg);
+        file.read(data, file_size_bytes);
+        data[file_size_bytes] = '\0';
+        file.close();
+        std::printf("%s\n", data);
+
+        
+    }
+
+};
+
 
 class D06a {
     public:
     std::vector<std::string> data;
-    int w, h;
+    int w = 0, h = 0;
 
     
     bool oob(int x, int y) {
@@ -23,16 +50,33 @@ class D06a {
         }
     }
 
+    uint64_t op(uint64_t a, uint64_t b, char op) {
+        if (op == '*') {
+            return a * b;
+        } else {
+            return a + b;
+        }
+    }
+
     void run() {
         loadData();
         std::cout << "dims:(" << w << "," << h << ")\n";
-        for (int y = 0; y < h; y += 1) {
+        std::vector<uint64_t> nums;
+        // char operator = '\0';
+        uint64_t sum = 0;
+        for (int x = 0; x < w; x += 1) {
+            char optr = get(x,4)[0];
+            
+            uint64_t a = std::strtoimax(get(x,0).c_str(),NULL,10);
+            a = op(a, std::strtoimax(get(x,1).c_str(),NULL,10),optr);
+            a = op(a, std::strtoimax(get(x,2).c_str(),NULL,10),optr);
+            a = op(a, std::strtoimax(get(x,3).c_str(),NULL,10),optr);
+            sum += a;
 
-            for (int x = 0; x < w; x += 1) {
-                std::cout << " " << get(x,y) ;       
-            }
-            std::cout << "\n";
+            
         }
+
+        std::cout << "sum : " << sum << "\n";
     }
     
 
@@ -43,13 +87,17 @@ class D06a {
         
         bool first_row = true;
         h = 0;
+        w = 0;
+        
         while (std::getline(file,line)) {
-            h =+ 1;
+            std::cout << "reading line " << h << "\n";
+      
+            h += 1;
             std::string current = "";
             for (size_t x = 0; x < line.length(); x += 1) {
                 char c = line[x];
                 if (c == ' ') {
-                    if (current.length() > 0 || x == line.length()-1) {
+                    if (current.length() > 0) {
                         data.push_back(current);
                         current =  "";
                     }
--- main.cpp
+++ main.cpp
@@ -8,9 +8,9 @@ int main() {
     // D04b d04b;
     // d04b.run();
 
-    D06a d06a;
+    D06b d06b;
     auto start = std::chrono::high_resolution_clock::now();
-    d06a.run();
+    d06b.run();
     auto end = std::chrono::high_resolution_clock::now();
     const std::chrono::duration<double> time_in_seconds{end - start};
     std::cout << "Time Taken: " << time_in_seconds << "\n";
